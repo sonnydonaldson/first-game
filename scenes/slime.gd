9b22de05edc18a570731cdac0640ec30cebe9f8f
sonnydonaldson <donaldsons@wsc.school.nz>
2026-09-07 11:14:27 +1200

    making the enmeys look like they achulary stay in the grouf

--- scenes/slime.gd
+++ scenes/slime.gd
@@ -1,12 +1,13 @@
 extends Node2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastleft
-@onready var raycastdown: RayCast2D = $raycastdown
+@onready var raycastdownright: RayCast2D = $raycastdownright
+@onready var raycastdownleft: RayCast2D = $raycastdownleft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 var direction = -1
-var SPEED = 50
+var SPEED = 40
 
 
 
@@ -17,7 +18,7 @@ func _process(delta: float) -> void:
 		direction = 1
 	elif ray_cast_right.is_colliding():
 		direction = -1
-	elif not raycastdown.is_colliding():
+	elif not raycastdownright.is_colliding() or not raycastdownleft.is_colliding():
 		direction *= -1
 	if direction == 1:
 		animated_sprite_2d.flip_h = false
